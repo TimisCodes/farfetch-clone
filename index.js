69a9a770b7deebce0546cc912f4cd3cc6e9390b7
Timilehin Olabisi <107751729+TimisCodes@users.noreply.github.com>
2024-09-19 20:46:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// var clicked = document.querySelector("button");
+
+// clicked.setAttribute("click", signup(){
+
+//     location.replace("file:///C:/Users/WORLDTECHT/Desktop/Software%20Engineer/Farfetch/signup.html")
+// })
+function login(){
+    location.replace("file:///C:/Users/WORLDTECHT/Desktop/Software%20Engineer/Farfetch/signup.html")
+}
